caada29a4bde6822ce9a0f977f3e5c068bcdd6a7
Valéria Campaner <vava.siqueira@hotmail.com>
2019-05-13 14:33:01 -0300

    implementamos a classe Usuário, com métodos de lista e busca

--- Class/Usuario.php
+++ Class/Usuario.php
@@ -54,6 +54,44 @@ class Usuario{
 			$this->setDtcadastro(new DateTime($row['dtcadastro']));
 		}
 
+	}/*   fim metodo loadById  */
+
+
+/*   metodo para  trazer todos  os dados da tabela  */
+	public static function getList(){//um método estático tem a vantagem de poder realizar uma consulta direta, sem instanciar o objeto
+
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_usuarios ORDER BY idusuario");
+	}/*   fim metodo loadById  */
+
+/*   metodo para  trazer fazer uma consulta específica */
+	public static function search($login){
+
+		$sql = new Sql();
+		return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+				':SEARCH'=>"%".$login."%"
+		));
+	}/*   fim método consulta  search*/
+
+	public function login($login, $password){
+		$sql = new Sql();//instancio a classe Sql
+
+		$result = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+			":LOGIN"=>$login,
+			":PASSWORD"=>$password
+		));//filtragem de dados
+
+		if (count($result) > 0 ){
+			$row = $result[0];
+			
+			$this->setIdusuario($row['idusuario']);
+			$this->setDeslogin($row['deslogin']);
+			$this->setDessenha($row['dessenha']);
+			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+		}else {
+			throw new Exception('Login e/ou senha inválidos.');
+		}
 	}
 
 	public function __toString(){
@@ -64,6 +102,8 @@ class Usuario{
 			"dtcadastro"=>$this->getDtcadastro()->format("d-m-Y H:i:s")
 		));
 	}
+
+	
 
 }
 
--- index.php
+++ index.php
@@ -2,11 +2,26 @@
 
 require_once("config.php");
 
-$user = new Usuario();
-
+//1ª - Retorna a consulta de apenas uma linha da tabela
+/*$user = new Usuario();
 $user->loadById(3);
+echo $user;*/
 
-echo $user;
+//2ª - Retorna todos os cadastros da tabela
+/*$lista = Usuario::getList();//sem necessidade de instanciar, chamando a classe Usuario :: e seu método getList
+echo json_encode($lista);*/
+
+//3ª - Retorna uma consulta específica
+/*$busca = Usuario::search("u");
+echo json_encode($busca);*/
+
+//4ª - Retorna login e senha
+$acesso = new Usuario();
+$acesso->login("user","12345");
+
+echo $acesso;
+
+
 /*$sql = new Sql();//instanciando a classe
 
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
